49b00ced8a7dba0ab8b29e6c601e1758b17efe99
mostafa-seni <mostafaayan886@gmail.com>
2026-08-29 00:19:37 +0300

    Remove token service registration in Program.cs
    This commit removes the line that adds the token service to the service container in `Program.cs`. The application will no longer register the token service during the service configuration process.

--- MapService/Map/Program.cs
+++ MapService/Map/Program.cs
@@ -17,7 +17,6 @@ namespace Map
             builder.Services.AddControllers();
             builder.Services.AddPersistenceServices(builder.Configuration);
             builder.Services.AddServices();
-            builder.Services.AddTokenService(builder.Configuration);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
